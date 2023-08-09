24456aff8f177e6da3e18e6117615faf60ac5e91
Muhammad Moin Raza Khan <muhammadmoin74@gmail.com>
2023-08-09 13:13:16 +0500

    Moving Zeros Problem and Solution

---
+++ src/main/kotlin/Mathematics/MovingZerosArray.kt
@@ -0,0 +1,30 @@
+package Mathematics
+
+class MovingZerosArray {
+    fun moveZeros(numbers: IntArray): IntArray{
+        for(i in numbers.indices){
+            for(j in numbers.indices){
+                if(j>i && numbers[i] == 0 && numbers[j] != 0){
+                    val temp = numbers[i]
+                    numbers[i] = numbers[j]
+                    numbers[j] = temp
+                }
+                else{
+                    j.inc()
+                }
+            }
+        }
+        return numbers
+    }
+}
+fun main(args: Array<String>){
+    var numbers = intArrayOf(0, 1, 0, 4, 12)
+    val movingZerosArray = MovingZerosArray()
+    print("Before Moving Zeros: ")
+    numbers.forEach { i -> print(""+i+" ") }
+    println()
+    numbers = movingZerosArray.moveZeros(numbers)
+    print("After Moving Zeros:  ")
+    numbers.forEach { i -> print(""+i+" ") }
+
+}
